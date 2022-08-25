76ce5dd6ac3ae3b69803a1528993e3334c99cc32
todelusion <todelusion.gmail.com>
2022-08-26 00:50:47 +0800

    f/add-datepickerHasRange_disable-datepickerCustom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-//DateNoRange
-import DatePicker from "./datepicker/DatePicker";
 
-const onChange = (time:{timestamp: number; dateString: string}) => {
-  console.log(time);
-  return time
-};
 
-//DateHasRange
 import DatepickerHasRrange from './datepickerHasRange/DatepickerHasRrange'
 
 export default function App() {
@@ -14,7 +7,6 @@ export default function App() {
   
   return (
     <div className="flex justify-center mt-20">
-      {/* <DatePicker onChange={onChange} /> */}
       <DatepickerHasRrange />
     </div>
   );
