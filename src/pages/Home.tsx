498ac6ed98ff6f11e3996825ddf1a47c2e03621e
todelusion <todelusion.gmail.com>
2022-08-30 00:31:23 +0800

    home/grid_done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import React, {Component} from "react";
 import { useApi } from "../hooks/useApi";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -5,6 +6,7 @@ import { Autoplay, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import useFetch from "../hooks/useFetch";
+import houseIcon from '../assets/houseIcon.svg'
 
 type item = {
   holidayPrice: number,
@@ -14,7 +16,7 @@ type item = {
   normalDayPrice: number
 }
 
-export default function Home() {
+export default function Home(): any{
   const baseUrl = useApi().baseUrl;
   const { data }: any = useFetch(`${baseUrl}/rooms`)
   if(Object.keys(data).length === 0)return
@@ -22,13 +24,28 @@ export default function Home() {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="text-white pointer-events-none select-none z-10 hidden">
-        Hello
+      <div className="pointer-events-none select-none z-10 py-28 px-32 h-full flex justify-between items-center">
+        <div className="flex flex-col justify-between h-full">
+          <img src={houseIcon} alt="LOGO" className="w-36"/>
+          <div className="text-white text-xs font-sans font-thin">
+            <h1 className="mb-4 font-normal">好室旅店。HOUSE HOTEL</h1>
+            <p className="mb-[6px]">花蓮縣花蓮市國聯一路1號</p>
+            <p className="mb-[6px]">03-8321155</p>
+            <p>HOUSE@HOTEL.COM</p>
+          </div>
+        </div>
+        <ul className="grid grid-cols-3 grid-rows-2 ml-32 gap-y-0 h-max">
+          {data.items.map((item: item) => {
+            return(
+              <li key={item.id} className='max-h-[275px]'><img src={item.imageUrl}  className='max-w-[275px] max-h-[275px] w-full h-full object-cover'/></li>
+            )
+          })}
+        </ul>
       </div>
       <Swiper pagination={{ clickable: true }} modules={[Autoplay, Pagination]} autoplay={{delay: 4000, disableOnInteraction: false}} className='absolute pointer-events-auto'>
         {data.items.map((item: item) => {
           return(
-            <SwiperSlide key={item.id} className='brightness-50 '><img src={`${item.imageUrl}`} alt="banner" width='1366'/></SwiperSlide>
+            <SwiperSlide key={item.id}><img src={item.imageUrl} alt="banner" width='1366' className='brightness-50'/></SwiperSlide>
           )
         })}
       </Swiper>
