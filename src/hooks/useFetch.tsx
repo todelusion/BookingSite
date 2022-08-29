3778b585aca3e9dc59c21d74300edfb2c6858e22
todelusion <todelusion.gmail.com>
2022-08-30 00:48:43 +0800

    prettier

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,27 +1,26 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 type T = {
-    success?: boolean,
-    items?: object[]
-  }
+  success?: boolean;
+  items?: object[];
+};
 
-const useFetch = (url:string) => {
+const useFetch = (url: string) => {
+  const [data, setData] = useState<T>({});
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(url, {
+        headers: {
+          Authorization:
+            "Bearer saSb3FzqHIjWaweZP9llX8Y9oixK9X7aZZq5jPyq9XFnFUdOdUwuZc5iTZwv",
+        },
+      });
+      if (typeof res.data === undefined) return;
+      setData(res.data);
+    };
+    fetchData();
+  }, []);
+  return { data };
+};
 
-    const [data, setData] = useState<T>({})
-	useEffect(() => {
-		const fetchData = async () => {
-            const res = await axios.get(url, {
-              headers: {
-              Authorization:
-                "Bearer saSb3FzqHIjWaweZP9llX8Y9oixK9X7aZZq5jPyq9XFnFUdOdUwuZc5iTZwv",
-              },
-            });
-            if(typeof res.data === undefined)return
-            setData(res.data);
-          }
-          fetchData()
-	},[])
-	return { data }
-}
-
-export default useFetch
+export default useFetch;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { useApi } from "../hooks/useApi";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -6,48 +6,67 @@ import { Autoplay, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import useFetch from "../hooks/useFetch";
-import houseIcon from '../assets/houseIcon.svg'
+import houseIcon from "../assets/houseIcon.svg";
 
 type item = {
-  holidayPrice: number,
-  id: string,
-  imageUrl: string,
-  name: string,
-  normalDayPrice: number
-}
+  holidayPrice: number;
+  id: string;
+  imageUrl: string;
+  name: string;
+  normalDayPrice: number;
+};
 
-export default function Home(): any{
+export default function Home(): any {
   const baseUrl = useApi().baseUrl;
-  const { data }: any = useFetch(`${baseUrl}/rooms`)
-  if(Object.keys(data).length === 0)return
-  console.log(data)
+  const { data }: any = useFetch(`${baseUrl}/rooms`);
+  if (Object.keys(data).length === 0) return;
+  console.log(data);
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="pointer-events-none select-none z-10 py-28 px-32 h-full flex justify-between items-center">
-        <div className="flex flex-col justify-between h-full">
-          <img src={houseIcon} alt="LOGO" className="w-36"/>
-          <div className="text-white text-xs font-sans font-thin">
+      <div className="pointer-events-none z-10 flex h-full select-none items-center justify-between py-28 px-32">
+        <div className="flex h-full flex-col justify-between">
+          <img src={houseIcon} alt="LOGO" className="w-36" />
+          <div className="font-sans text-xs font-thin text-white">
             <h1 className="mb-4 font-normal">好室旅店。HOUSE HOTEL</h1>
             <p className="mb-[6px]">花蓮縣花蓮市國聯一路1號</p>
             <p className="mb-[6px]">03-8321155</p>
             <p>HOUSE@HOTEL.COM</p>
           </div>
         </div>
-        <ul className="grid grid-cols-3 grid-rows-2 ml-32 gap-y-0 h-max pointer-events-auto">
+        <ul className="pointer-events-auto ml-32 grid h-max grid-cols-3 grid-rows-2 gap-y-0">
           {data.items.map((item: item) => {
-            return(
-              <li key={item.id} className='relative max-h-[275px] hover:before:content-["123"] before:text-white before:font-thin before:flex before:justify-center before:items-center before:absolute before:left-0 before:right-0  before:bottom-0 before:bg-primary/60'>
-                <img src={item.imageUrl}  className='max-w-[275px] max-h-[275px] w-full h-full object-cover '/></li>
-            )
+            return (
+              <li
+                key={item.id}
+                className='relative max-h-[275px] before:absolute before:left-0 before:right-0 before:bottom-0 before:flex before:items-center before:justify-center before:bg-primary/60 before:font-thin  before:text-white hover:before:content-["123"]'
+              >
+                <img
+                  src={item.imageUrl}
+                  className="h-full max-h-[275px] w-full max-w-[275px] object-cover "
+                />
+              </li>
+            );
           })}
         </ul>
       </div>
-      <Swiper pagination={{ clickable: true }} modules={[Autoplay, Pagination]} autoplay={{delay: 4000, disableOnInteraction: false}} className='absolute pointer-events-auto'>
+      <Swiper
+        pagination={{ clickable: true }}
+        modules={[Autoplay, Pagination]}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        className="pointer-events-auto absolute"
+      >
         {data.items.map((item: item) => {
-          return(
-            <SwiperSlide key={item.id}><img src={item.imageUrl} alt="banner" width='1366' className='brightness-50'/></SwiperSlide>
-          )
+          return (
+            <SwiperSlide key={item.id}>
+              <img
+                src={item.imageUrl}
+                alt="banner"
+                width="1366"
+                className="brightness-50"
+              />
+            </SwiperSlide>
+          );
         })}
       </Swiper>
     </div>
