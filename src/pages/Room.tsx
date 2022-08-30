53339133c7c7ad74f98c9e0c96af9c5bb3991ca6
todelusion <todelusion.gmail.com>
2022-08-30 19:47:51 +0800

    [prettier]Rooms/customize swiper

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -10,13 +10,13 @@ export default function Room() {
   console.log(params);
   return (
     <div className="flex h-screen">
-      <section className="pointer-events-none flex h-full w-1/3 select-none items-center justify-center overflow-clip border-2 border-black">
+      <section className="pointer-events-none flex h-full w-full max-w-xl select-none items-center justify-center">
         <div className="z-10 mt-10">
           <p className="mb-4 text-center font-sans text-4xl text-primary">
             $1,380 <span className="px-4 text-xl">/</span>
             <span className="text-xl">1晚</span>
           </p>
-          <button className="bg-primary py-2 px-14 text-white">
+          <button className="pointer-events-auto bg-primary py-2 px-14 text-white">
             Booking now
           </button>
         </div>
@@ -24,19 +24,13 @@ export default function Room() {
           pagination={{ clickable: true }}
           modules={[Autoplay, Pagination]}
           autoplay={{ delay: 4000, disableOnInteraction: false }}
-          className="swiper-rooms pointer-events-auto absolute select-none"
+          className="swiper-rooms pointer-events-auto absolute max-w-xl select-none"
         >
-          <SwiperSlide>
-            <img
-              src="https://images.unsplash.com/photo-1551776235-dde6d482980b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80"
-              className="brightness-50"
-            />
+          <SwiperSlide className="swiper-pseudo">
+            <img src="https://images.unsplash.com/photo-1551776235-dde6d482980b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80" />
           </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://images.unsplash.com/photo-1551776235-dde6d482980b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80"
-              className="brightness-50"
-            />
+          <SwiperSlide className="swiper-pseudo">
+            <img src="https://images.unsplash.com/photo-1551776235-dde6d482980b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80" />
           </SwiperSlide>
         </Swiper>
       </section>
